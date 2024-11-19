6e6251ad44507abe0cb98a6bbe86e6619b8fb9ef
Emilio Palumbo <emiliopalumbo@gmail.com>
2024-11-19 10:50:31 +0100

    Add border for better reading

--- init.lua
+++ init.lua
@@ -829,6 +829,10 @@ require('lazy').setup({
           { name = 'luasnip' },
           { name = 'path' },
         },
+        window = {
+          completion = { border = 'rounded' },
+          documentation = { border = 'rounded' },
+        },
       }
     end,
   },
