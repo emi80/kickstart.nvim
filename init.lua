555f051439f562cf6921a60a1ab685455028318a
Emilio Palumbo <emiliopalumbo@gmail.com>
2024-12-16 16:51:11 +0100

    Add snakemake stuff

--- init.lua
+++ init.lua
@@ -658,6 +658,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'snakefmt', -- Used to format Snakemake code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -712,6 +713,7 @@ require('lazy').setup({
         lua = { 'stylua' },
         zig = { 'zigfmt' },
         python = { 'ruff_fix', 'ruff_format', 'ruff_organize_imports' },
+        snakemake = { 'snakefmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
